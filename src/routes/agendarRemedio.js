0c079ccc717c36301adf3999c013e331a076468a
Gleidson <79335967+gleidsondevC@users.noreply.github.com>
2022-12-09 19:11:17 -0300

    fix agendarRemedio

--- src/routes/agendarRemedio.js
+++ src/routes/agendarRemedio.js
@@ -4,19 +4,11 @@ const agendarRemedio = async (req, res) => {
   try{
     const body = req.body;
     if(!(body.dataRemedio && body.nomeRemedio)) throw new Error("400");
-    const {error} =  await supabase.from('remedios').insert({nome: body.nomeRemedio, agendado: body.agendado});
-    if(error === null){
-      res.status(200).send({status: "ok"});
-    }else{
-      res.status(500).send({status: "Erro ao encontrar dados"});
-    }
+    await supabase.from('remedios').insert({nome: body.nomeRemedio, agendado: body.agendado});
+    res.status(200).send({status: "ok"});
   }catch(error){
-    if(error instanceof Error) {
-      const resStatus = Number(error.message);
-      res.status(resStatus).send({error: "Dados inválidos"})
-    }
-    res.status(500).send({error : "Erro inesperado"});
-  }
+    res.status(500).send({error : error});
+   }
   }
 
   exports.agendarRemedio = agendarRemedio;
